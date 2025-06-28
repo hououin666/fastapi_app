1c023be0b980e16012c8fc8c495bd834beb85ad5
redlikeroses <devmaksim2006@gmail.com>
2025-06-28 14:30:35 +0300

    update to orm order_product_association

--- core/models/order.py
+++ core/models/order.py
@@ -5,7 +5,7 @@ from sqlalchemy import func
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from core.models import Base, Item
-from core.models.order_product_association import order_product_association_table
+
 
 if TYPE_CHECKING:
     from core.models import Item
@@ -15,5 +15,5 @@ class Order(Base):
 
     promocode: Mapped[str | None]
     created_at: Mapped[datetime] = mapped_column(server_default=func.now(), default=datetime.utcnow)
-    products: Mapped[list['Item']] = relationship(secondary=order_product_association_table, back_populates='orders')
+    products: Mapped[list['Item']] = relationship(secondary='order_product_association', back_populates='orders')
 
--- core/models/order_product_association.py
+++ core/models/order_product_association.py
@@ -1,12 +1,28 @@
 from sqlalchemy import Table, Integer, Column, ForeignKey, UniqueConstraint
+from sqlalchemy.orm import Mapped, mapped_column
 
 from core.models import Base
 
-order_product_association_table = Table(
-    'order_product_association',
-    Base.metadata,
-    Column('id', Integer, primary_key=True),
-    Column('order_id', ForeignKey('orders.id'), nullable=False),
-    Column('item_id', ForeignKey('items.id'), nullable=False),
-    UniqueConstraint('order_id', 'item_id', name='idx_unique_order_product')
-)
+# order_product_association_table = Table(
+#     'order_product_association',
+#     Base.metadata,
+#     Column('id', Integer, primary_key=True),
+#     Column('order_id', ForeignKey('orders.id'), nullable=False),
+#     Column('item_id', ForeignKey('items.id'), nullable=False),
+#     UniqueConstraint('order_id', 'item_id', name='idx_unique_order_product')
+# )
+
+
+class OrderProductAssociation(Base):
+    __tablename__ = 'order_product_association'
+    __table_args__ = (
+        UniqueConstraint(
+            'order_id',
+            'item_id',
+            name='idx_unique_order_product'
+        ),
+    )
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    order_id: Mapped[int] = mapped_column(ForeignKey('orders.id'))
+    item_id: Mapped[int] = mapped_column(ForeignKey('items.id'))
