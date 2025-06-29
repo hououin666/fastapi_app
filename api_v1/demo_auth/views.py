2622de8078ef1054e9497e14bbebd0f6106418a0
redlikeroses <devmaksim2006@gmail.com>
2025-06-29 18:22:52 +0300

    demo func for Basic Auth with validate

--- api_v1/demo_auth/views.py
+++ api_v1/demo_auth/views.py
@@ -1,7 +1,8 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from fastapi.security import HTTPBasicCredentials, HTTPBasic
+from starlette.status import HTTP_401_UNAUTHORIZED
 
 router = APIRouter(prefix='/demo-auth', tags=['Demo Auth'])
 
@@ -14,4 +15,26 @@ def demo_basic_auth_credentials(credentials: Annotated[HTTPBasicCredentials, Dep
         'message': 'Hi!',
         'username': credentials.username,
         'password': credentials.password,
+    }
+
+
+usernames_to_passwords = {
+    'admin': 'admin',
+    'username': 'username',
+}
+
+
+def get_auth_user_username(credentials: Annotated[HTTPBasicCredentials, Depends(security)]):
+    if credentials.username not in usernames_to_passwords:
+        raise HTTPException(status_code=HTTP_401_UNAUTHORIZED, detail='invalid username or password')
+    if credentials.password != usernames_to_passwords[credentials.username]:
+        raise HTTPException(status_code=HTTP_401_UNAUTHORIZED, detail='invalid username or password')
+    return credentials.username
+
+
+@router.get('/basic-auth-username')
+def demo_basic_auth_username(auth_username: str = Depends(get_auth_user_username)):
+    return {
+        'message': f'Hi, {auth_username}!',
+        'username': auth_username,
     }
